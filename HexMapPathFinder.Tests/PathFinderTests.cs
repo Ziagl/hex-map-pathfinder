9b0df794a1acc9e703bcd338f47538cec76aa062
Ziagl <office@ziegelwanger-edv.at>
2024-09-30 15:02:03 +0200

    updates

--- HexMapPathFinder.Tests/PathFinderTests.cs
+++ HexMapPathFinder.Tests/PathFinderTests.cs
@@ -16,4 +16,70 @@ public class PathFinderTests
         var path = pathFinder.ComputePath(new CubeCoordinates(0, 0, 0), new CubeCoordinates(2, 3, -5), 0);
         Assert.Equal(7, path.Count);
     }
+
+    [Fact]
+    public void TestComputePathOffsetCoordinates()
+    {
+        List<int> costMap = new() { 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1 };
+        var grid = HexGrid.InitializeGrid<Tile>(4, 4);
+        HexTile tile = new HexTile() { Coordinates = new CubeCoordinates(0, 0, 0) };
+        var pathFinder = new PathFinder(new List<List<int>>() { costMap }, 4, 4);
+        var path = pathFinder.ComputePathOffsetCoordinates(new OffsetCoordinates(0, 0), new OffsetCoordinates(3, 3), 0);
+        Assert.Equal(7, path.Count);
+    }
+
+    [Fact]
+    public void TestFindComplexPath()
+    {
+        List<int> costMap = new() { 1, 1, 2, 3, 1, 2, 1, 3, 2, 4, 8, 1, 3, 1, 2, 1 };
+        var grid = HexGrid.InitializeGrid<Tile>(4, 4);
+        HexTile tile = new HexTile() { Coordinates = new CubeCoordinates(0, 0, 0) };
+        var pathFinder = new PathFinder(new List<List<int>>() { costMap }, 4, 4);
+        var path = pathFinder.ComputePathOffsetCoordinates(new OffsetCoordinates(0, 0), new OffsetCoordinates(3, 3), 0);
+        Assert.Equal(6, path.Count);
+    }
+
+    [Fact]
+    public void TestNoPossiblePath()
+    {
+        List<int> costMap = new() { 1, 1, 2, 1, 1, 2, 3, 3, 2, 2, 8, 0, 1, 1, 0, 1 };
+        var grid = HexGrid.InitializeGrid<Tile>(4, 4);
+        HexTile tile = new HexTile() { Coordinates = new CubeCoordinates(0, 0, 0) };
+        var pathFinder = new PathFinder(new List<List<int>>() { costMap }, 4, 4);
+        var path = pathFinder.ComputePathOffsetCoordinates(new OffsetCoordinates(0, 0), new OffsetCoordinates(3, 3), 0);
+        Assert.Empty(path);
+    }
+
+    [Fact]
+    public void TestReachableTiles()
+    {
+        List<int> costMap = new() { 1, 1, 2, 3, 1, 2, 1, 3, 2, 4, 8, 1, 3, 1, 2, 1 };
+        var grid = HexGrid.InitializeGrid<Tile>(4, 4);
+        HexTile tile = new HexTile() { Coordinates = new CubeCoordinates(0, 0, 0) };
+        var pathFinder = new PathFinder(new List<List<int>>() { costMap }, 4, 4);
+        var reachableTiles = pathFinder.ReachableTiles(new CubeCoordinates(0, 0, 0), 2, 0);
+        Assert.Equal(7, reachableTiles.Count);
+    }
+
+    [Fact]
+    public void TestReachableTiles2()
+    {
+        List<int> costMap = new() { 8, 8, 8, 3, 8, 1, 1, 4, 8, 8, 1, 1, 3, 9, 1, 2 };
+        var grid = HexGrid.InitializeGrid<Tile>(4, 4);
+        HexTile tile = new HexTile() { Coordinates = new CubeCoordinates(0, 0, 0) };
+        var pathFinder = new PathFinder(new List<List<int>>() { costMap }, 4, 4);
+        var reachableTiles = pathFinder.ReachableTiles(new CubeCoordinates(1, 1,-2), 2, 0);
+        Assert.Equal(12, reachableTiles.Count);
+    }
+
+    [Fact]
+    public void TestWrongLayer()
+    {
+        List<int> costMap = new() { 8, 8, 8, 3, 8, 1, 1, 4, 8, 8, 1, 1, 3, 9, 1, 2 };
+        var grid = HexGrid.InitializeGrid<Tile>(4, 4);
+        HexTile tile = new HexTile() { Coordinates = new CubeCoordinates(0, 0, 0) };
+        var pathFinder = new PathFinder(new List<List<int>>() { costMap }, 4, 4);
+        var reachableTiles = pathFinder.ReachableTiles(new CubeCoordinates(1, 1, -2), 2, 1);
+        Assert.Empty(reachableTiles);
+    }
 }
--- HexMapPathFinder/PathFinder.cs
+++ HexMapPathFinder/PathFinder.cs
@@ -13,6 +13,13 @@ public class PathFinder
         _map.Columns = columns;
     }
 
+    /// <summary>
+    /// Computes path with lowest costs from start to end (A* algorithm).
+    /// </summary>
+    /// <param name="start">start coordinates</param>
+    /// <param name="end">end coordinates</param>
+    /// <param name="layerIndex">layer index</param>
+    /// <returns>path as cube coordinates or empty path if no path was found or layer is out of bounds</returns>
     public List<CubeCoordinates> ComputePath(CubeCoordinates start, CubeCoordinates end, int layerIndex)
     {
         if (layerIndex < 0 || layerIndex >= _map.Map.Count)
@@ -71,7 +78,7 @@ public class PathFinder
                     walkableNeighbor.MovementCost = tile.MovementCost + tileMovementCost;
                     walkableNeighbor.EstimatedMovementCost = CubeCoordinates.Distance(walkableNeighbor.Coordinates, end);
                     walkableNeighbor.Sum = walkableNeighbor.MovementCost + walkableNeighbor.EstimatedMovementCost;
-                    openList.Insert(0, walkableNeighbor);
+                    openList.Add(walkableNeighbor);
                 }
                 // if neighbor is in open list and has a lower cost, update it
                 else
@@ -82,7 +89,7 @@ public class PathFinder
                     if (existingTile is not null && existingTile.MovementCost > tile.MovementCost + tileMovementCost)
                     {
                         existingTile.MovementCost = tile.MovementCost + tileMovementCost;
-                        existingTile.EstimatedMovementCost = CubeCoordinates.Distance(existingTile.Coordinates, end);
+                        existingTile.EstimatedMovementCost = CubeCoordinates.Distance(walkableNeighbor.Coordinates, end);
                         existingTile.Sum = existingTile.MovementCost + existingTile.EstimatedMovementCost;
                     }
                 }
@@ -114,7 +121,6 @@ public class PathFinder
                         if (nextTile is not null && neighbor.MovementCost < current.MovementCost)
                         {
                             current = nextTile;
-                            break;
                         }
                     }    
                 }
@@ -123,5 +129,92 @@ public class PathFinder
         }
         path.Reverse();
         return path;
+    }
+
+    /// <summary>
+    /// Same as computePath, but with interface for offset coordinates
+    /// </summary>
+    /// <param name="start">start coordinates</param>
+    /// <param name="end">end coordinates</param>
+    /// <param name="layerIndex">layer index</param>
+    /// <returns>path as offset coordinates or empty path if no path was found or layer is out of bounds</returns>
+    public List<OffsetCoordinates> ComputePathOffsetCoordinates(OffsetCoordinates start, OffsetCoordinates end, int layerIndex)
+    {
+        List<OffsetCoordinates> path = new();
+        // compute path
+        var pathCube = ComputePath(start.ToCubic(), end.ToCubic(), layerIndex);
+        // convert resulting path back to output offset coordinates
+        if(pathCube.Count > 0)
+        {
+            pathCube.ForEach(coord => path.Add(coord.ToOffset()));
+        }
+        return path;
+    }
+
+    /// <summary>
+    /// Returns all tiles that are in range
+    /// </summary>
+    /// <param name="start">start coordinates</param>
+    /// <param name="maxCost">maximum cost</param>
+    /// <param name="layerIndex">layer index</param>
+    /// <returns>reachable tiles as cube coordinates or empty path if layer is out of bounds</returns>
+    public List<CubeCoordinates> ReachableTiles(CubeCoordinates start, int maxCost, int layerIndex)
+    {
+        if (layerIndex < 0 || layerIndex >= _map.Map.Count)
+        {
+            return new List<CubeCoordinates>();
+        }
+        // create empty grid
+        List<Tile> grid = HexGrid.InitializeGrid<Tile>(_map.Rows, _map.Columns);
+        List<CubeCoordinates> reachableTiles = new();
+        List<Tile> openList = new();
+        List<Tile> closedList = new();
+        openList.Add(new Tile()
+        {
+            Coordinates = start,
+            MovementCost = 0,
+        });
+        // compute AStar algorith
+        int loopMax = this.MAXLOOPS;
+        do
+        {
+            // remove tile from open list
+            var tile = openList.First();
+            openList.RemoveAt(0);
+            // and add it to closed list
+            closedList.Add(tile);
+            // if start tile is not passable, break
+            if (_map.Map[layerIndex][start.ToOffset().x * _map.Rows + start.ToOffset().y] == 0)
+            {
+                break;
+            }
+            // get neighbors walkable neighbors
+            var neighbors = tile.Neighbors(grid.Cast<HexTile>().ToList(), _map.Rows, _map.Columns);
+            var walkableNeighbors = Utils.WalkableNeighbors(neighbors, _map.Map[layerIndex], _map.Columns).Cast<Tile>().ToList();
+            // for every walkable neighbor
+            foreach (var walkableNeighbor in walkableNeighbors)
+            {
+                // if neighbor is in closed list, skip it
+                if (closedList.Contains(walkableNeighbor))
+                {
+                    continue;
+                }
+                // if neighbor is not in open list, add it
+                if (!openList.Contains(walkableNeighbor))
+                {
+                    var offset = walkableNeighbor.Coordinates.ToOffset();
+                    int tileMovementCost = _map.Map[layerIndex][offset.y * _map.Columns + offset.x];
+                    walkableNeighbor.MovementCost = tile.MovementCost + tileMovementCost;
+                    if (tile.MovementCost < maxCost)
+                    {
+                        openList.Insert(0, walkableNeighbor);
+                    }
+                }
+            }
+            --loopMax;
+        }while(openList.Count > 0 && loopMax > 0);
+        // fill reachable tiles
+        closedList.ForEach(tile => reachableTiles.Add(tile.Coordinates));
+        return reachableTiles;
     }
 }
